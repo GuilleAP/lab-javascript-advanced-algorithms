3a84db711c1a84cecfb62a17ac1443e191199bbe
Guillem Alerany Perez <guillemfata@gmail.com>
2022-06-08 16:07:28 +0200

    Done!

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -6,22 +6,48 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if(this.queueControl.length === this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if(this.queueControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(this.queueControl.length < 10) {
+      this.queueControl.push(item);
+    } else {
+      throw new Error('QUEUE_OVERFLOW');
+    }
+
+    return this.queueControl;
   }
 
   dequeue() {
     // ... your code goes here
+    let item;
+    if(this.queueControl.length !== 0) {
+      item = this.queueControl[0];
+      this.queueControl.shift();
+    } else {
+      throw new Error('QUEUE_UNDERFLOW');
+    }
+    return item;
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl;
   }  
 }
 
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,22 +6,50 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if(this.stackControl.length === 0) 
+      return true
+    else 
+      return false
   }
 
   isEmpty() {
     // ... your code goes here
+    if(this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
     // ... your code goes here
+    if(this.stackControl.length < 10) {
+      this.stackControl.push(item);
+
+    } else {
+      throw new Error('STACK_OVERFLOW');
+      
+    }
+
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    let item;
+    if(this.stackControl.length !== 0) {
+      item = this.stackControl[this.stackControl.length - 1];
+      this.stackControl.slice();
+    } else {
+      throw new Error('STACK_UNDERFLOW');
+
+    }
+    return item;
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
